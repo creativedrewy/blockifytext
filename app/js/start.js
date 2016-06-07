68aae10337df9d26bd7fe23a36b503f7f2fc40d9
Andrew <creativedrewy@gmail.com>
2016-06-07 12:41:02 -0500

    Get vendor three js dependency working

--- app/js/start.js
+++ app/js/start.js
@@ -1,4 +1,8 @@
-require({ }, [ 'BlockifyText' ], function(lib) {
+require({ 
+    paths: {
+        'three': "vendor/three"
+    }
+}, [ 'BlockifyText' ], function(lib) {
     var blockifier = new lib.BlockifyText();
     blockifier.run();
 }); 
