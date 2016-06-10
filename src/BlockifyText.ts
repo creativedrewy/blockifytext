85d41dc3ea4ce6810733bdad956da27fbe51a191
Andrew <creativedrewy@gmail.com>
2016-06-10 15:52:22 -0500

    More word animation to function; variable refactor

--- src/BlockifyText.ts
+++ src/BlockifyText.ts
@@ -13,18 +13,19 @@ export class BlockifyText {
     private width = 900;
     private height = 400;
 
-    private mainRenderer:_three.WebGLRenderer;
-    private mainScene:_three.Scene;
-    private mainCamera:_three.Camera;
+    private rendererMain: _three.WebGLRenderer;
+    private sceneMain: _three.Scene;
+    private cameraMain: _three.Camera;
+    private wordMain: _three.Object3D;
 
     constructor() {
-        this.mainScene = new _three.Scene();
+        this.sceneMain = new _three.Scene();
 
-        this.mainCamera = new _three.PerspectiveCamera(75, this.width / this.height, 1, 10000);
-        this.mainCamera.position.z = 325;
-        this.mainRenderer = new _three.WebGLRenderer({ antialias: true });
-        this.mainRenderer.setSize(this.width, this.height);
-        document.body.appendChild(this.mainRenderer.domElement);
+        this.cameraMain = new _three.PerspectiveCamera(75, this.width / this.height, 1, 10000);
+        this.cameraMain.position.z = 325;
+        this.rendererMain = new _three.WebGLRenderer({ antialias: true });
+        this.rendererMain.setSize(this.width, this.height);
+        document.body.appendChild(this.rendererMain.domElement);
         
         this.setupLights();
 
@@ -33,46 +34,50 @@ export class BlockifyText {
 
         Observable.zip(meshLoader.loadBlock3dData(), fontService.loadFontData(BlockifyText.FONT_04b25))
             .subscribe((result) => {
-                var word = fontService.generate3dWord("creativedrewy");
-                this.mainScene.add(word);
+                this.wordMain = fontService.generate3dWord("creativedrewy");
+                this.sceneMain.add(this.wordMain);
 
-                word.rotation.x = -Math.PI / 22;
-                word.rotation.y = -Math.PI / 40;
-                word.rotation.z = -Math.PI / 40;
-
-                var timeline = new TimelineMax({ repeat: -1, yoyo: true });
-                timeline.add(TweenLite.to(word.rotation, 7, { x: Math.PI / 22, z: Math.PI / 40, ease: Quad.easeInOut }));
-                timeline.add(TweenLite.to(word.rotation, 7, { y: Math.PI / 40, ease: Quad.easeInOut }));
-                timeline.add(TweenLite.to(word.rotation, 7, { x: -Math.PI / 22, z: -Math.PI / 40, ease: Quad.easeInOut }));
-                timeline.add(TweenLite.to(word.rotation, 7, { y: -Math.PI / 40, ease: Quad.easeInOut }));
+                this.animateWord();
             });
+    }
+
+    animateWord() {
+        this.wordMain.rotation.x = -Math.PI / 22;
+        this.wordMain.rotation.y = -Math.PI / 40;
+        this.wordMain.rotation.z = -Math.PI / 40;
+
+        var timeline = new TimelineMax({ repeat: -1, yoyo: true });
+        timeline.add(TweenLite.to(this.wordMain.rotation, 7, { x: Math.PI / 22, z: Math.PI / 40, ease: Quad.easeInOut }));
+        timeline.add(TweenLite.to(this.wordMain.rotation, 7, { y: Math.PI / 40, ease: Quad.easeInOut }));
+        timeline.add(TweenLite.to(this.wordMain.rotation, 7, { x: -Math.PI / 22, z: -Math.PI / 40, ease: Quad.easeInOut }));
+        timeline.add(TweenLite.to(this.wordMain.rotation, 7, { y: -Math.PI / 40, ease: Quad.easeInOut }));
     }
 
     setupLights() {
         var light1 = new _three.PointLight(0xffffff, 1.0);
         light1.position.set(300, 200, -300);
         light1.castShadow = true;
-        this.mainScene.add(light1);
+        this.sceneMain.add(light1);
 
         var light2 = new _three.PointLight(0xffffff, 1.0);
         light2.position.set(-300, -200, -300);
         light2.castShadow = true;
-        this.mainScene.add(light2);
+        this.sceneMain.add(light2);
 
         var light3 = new _three.PointLight(0xffffff, 1.0);
         light3.position.set(-300, 200, 300);
         light3.castShadow = true;
-        this.mainScene.add(light3);
+        this.sceneMain.add(light3);
 
         var light4 = new _three.PointLight(0xffffff, 1.0);
         light4.position.set(300, -200, 300);
         light4.castShadow = true;
-        this.mainScene.add(light4);
+        this.sceneMain.add(light4);
     }
 
     run() {
         setInterval(() => {
-            this.mainRenderer.render(this.mainScene, this.mainCamera);
+            this.rendererMain.render(this.sceneMain, this.cameraMain);
         }, 50);
     }
 }
